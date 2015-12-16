a8fdda15978cbc1bd55114c6a6cfeded2fb8bd88
Łukasz Chlebda <lukaszchlebda@gmail.com>
2015-12-16 20:54:19 +0100

    Initial commit

---
+++ src/Tutor.java
@@ -0,0 +1,16 @@
+/**
+ * Created by Łukasz on 16.12.2015.
+ */
+public class Tutor {
+	public void log(double message) {
+		System.out.println(message);
+	}
+
+	public void log(String message) {
+		System.out.println(message);
+	}
+
+	public void log(Object o) {
+		System.out.println(o);
+	}
+}
